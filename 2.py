cb16f18185287dec93b4dbb7d810bceef607ca47
kavipriya1829 <52239962+kavipriya1829@users.noreply.github.com>
2019-06-26 15:49:49 +0530

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,7 +1,8 @@
 num=int(input())
-if(num%2==0):
-  print("Even")
-elif(num%2==1):
-  print("Odd")
+if(num>=0):
+    if(num%2==0):
+        print("Even")
+    else(num%2==1):
+        print("Odd")
 else:
   print("invalid")
