c0f9fd0ae47b1e06753d7582dc2b320c3494c01d
kavipriya1829 <52239962+kavipriya1829@users.noreply.github.com>
2019-06-27 10:56:40 +0530

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,7 @@
+ch=input()
+if(ch=='a'or ch=='e'or ch=='i'or ch=='o'or ch=='u'or ch=='A'or ch=='E'or ch=="I"or ch=='O'or ch=='U'):
+  print("vowel")
+elif(ch=='b'or ch=='c'or ch=='d'or ch=='f'or ch=='j'or ch=='k'or ch=='l'or ch=="m"or ch=='n'or ch=='p'or ch=='q'or ch=='r'or ch=='s'or ch=='t'or ch=='v'or ch=='w'or ch=='x'or ch=="y"or ch=='z'or ch=='B'or ch=='C'or ch=='D'or ch=='F'or ch=='J'or ch=='K'or ch=='L'or ch=="M"or ch=='N'or ch=='P'or ch=='Q'or ch=='R'or ch=='S'or ch=='T'or ch=='V'or ch=='W'or ch=='X'or ch=="Y"or ch=='Z' ):
+  print("consonant")
+else:
+  print("invalid")
